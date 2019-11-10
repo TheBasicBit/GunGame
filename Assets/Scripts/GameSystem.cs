91c7d706a23e9d729acf46ab44e308e89334f9e1
Alex <afiesseler@gmail.com>
2019-11-10 02:00:20 +0100

    GameSystem

--- Assets/Scripts/GameSystem.cs
+++ Assets/Scripts/GameSystem.cs
@@ -5,8 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Diagnostics;
 using UnityEngine;
-
-using Console = UnityEngine.Debug;
+using Debug = UnityEngine.Debug;
 
 public static class GameSystem
 {
@@ -26,8 +25,7 @@ public static class GameSystem
     {
         if (ClientKeepAlive.Elapsed.TotalSeconds >= 1)
         {
-            Client.Power();
-            ClientKeepAlive.Restart();
+            //TODO: Send "KeepAlive"-Packet
         }
     }
 
@@ -44,57 +42,5 @@ public static class GameSystem
     public static void Destroy(this MonoBehaviour script)
     {
         script.gameObject.Destroy();
-    }
-}
-
-public class Packet
-{
-    private readonly byte[] buffer = new byte[128];
-
-    public static implicit operator byte[](Packet packet)
-    {
-        return packet.buffer;
-    }
-
-    public ushort ID
-    {
-        get
-        {
-            return BitConverter.ToUInt16(new byte[] { buffer[0], buffer[1] }, 0);
-        }
-
-        set
-        {
-            byte[] id = BitConverter.GetBytes(value);
-            buffer[0] = id[0];
-            buffer[1] = id[1];
-        }
-    }
-}
-
-public static class Client
-{
-    private static readonly TcpClient client = new TcpClient();
-
-    public static bool Connect(string host)
-    {
-        try
-        {
-            string address = host.Split(':')[0];
-            string port = host.Split(':')[1];
-
-            client.Connect(new IPEndPoint(IPAddress.Parse(address), int.Parse(port)));
-
-            return true;
-        }
-        catch
-        {
-            return false;
-        }
-    }
-
-    public static void Power()
-    {
-        Console.Log("Client: KeepAlive");
     }
 }
