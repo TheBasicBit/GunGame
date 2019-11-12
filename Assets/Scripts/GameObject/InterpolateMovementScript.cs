b7c843449c3a2b76a3586ba673a78d7ffc4169b5
Alex <afiesseler@gmail.com>
2019-11-12 20:32:01 +0100

    Script fix

---
+++ Assets/Scripts/GameObject/InterpolateMovementScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterpolateMovementScript : MonoBehaviour
+{
+    private float timer;
+    private float progress;
+    private Vector3 difference;
+    private Vector3 start;
+    private float time;
+    private Vector3 point;
+
+    public void Start()
+    {
+        point = transform.position;
+    }
+
+    public void Update()
+    {
+        if (timer <= time)
+        {
+            timer += Time.deltaTime;
+
+            progress = timer / time;
+            transform.position = start + difference * progress;
+        }
+    }
+
+    public void MoveTo(Vector3 pos, float seconds)
+    {
+        time = seconds;
+        point = pos;
+        start = transform.position;
+        difference = point - start;
+    }
+}
