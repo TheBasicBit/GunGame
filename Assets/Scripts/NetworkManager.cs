3b8c4097d554fc6e4269e7234ff8c77761af1c73
Alex <afiesseler@gmail.com>
2019-11-19 20:44:15 +0100

    Fix

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -74,7 +74,7 @@ public static class NetworkManager
         {
             GameSystem.RunSync(new Action(() =>
             {
-                Vector3 startPosition = new Vector3(bulletCreatePacket.posX, bulletCreatePacket.posY, bulletCreatePacket.posZ);
+                Vector3 startPosition = new Vector3(bulletCreatePacket.rotX, bulletCreatePacket.rotY, bulletCreatePacket.rotZ);
                 GameObject obj = GameSystem.CreateObject(GameSystem.Player.bulletPrefab, startPosition, GameSystem.PlayerCamera.transform.rotation);
                 Bullet bullet = obj.GetComponent<Bullet>();
                 bullet.startPosition = startPosition;
