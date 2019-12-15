87ffc4520782d999423677bf6ad67c9f5f8bb3d9
Felix Kranz <officialfelixkranz@gmx.de>
2019-12-15 01:28:27 +0100

    Character fix

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -47,7 +47,7 @@ public static class NetworkManager
                     OtherPlayer otherPlayer = OtherPlayer.OtherPlayers[clientPositionPacket.clientId];
                     otherPlayer.MoveTo(new Vector3(clientPositionPacket.posX, clientPositionPacket.posY, clientPositionPacket.posZ), new Vector3(0, clientPositionPacket.rotY, 0), 0.1f);
 
-                    GameObject rotBone = otherPlayer.GetChildWithName("mesh_otherPlayer/Test Rig/spine/spine.001/spine.002/spine.003/RotBone");
+                    GameObject rotBone = otherPlayer.GetChildWithName("mesh_otherPlayer/Armature/root/DEF.spine/DEF.chest/rotBone");
                     InterpolateMovementScript interpolateMovementScript = rotBone.GetComponent<InterpolateMovementScript>();
                     interpolateMovementScript.MoveLocalEulerAnglesTo(new Vector3(clientPositionPacket.rotX, 0, clientPositionPacket.rotZ), 0.1f);
                 }
