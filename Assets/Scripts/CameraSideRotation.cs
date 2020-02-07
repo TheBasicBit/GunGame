3c93f4ae6276cc124c50ebdb07f53b25a39042e4
Alex <afiesseler@gmail.com>
2020-02-07 23:08:17 +0100

    CameraRotationScript

---
+++ Assets/Scripts/CameraSideRotation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace BlindDeer.Game.PiouPiou
+{
+    public class CameraSideRotation : MonoBehaviour
+    {
+        public float speed = 1;
+
+        public void Update()
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + (Time.deltaTime * speed), transform.eulerAngles.z);
+        }
+    }
+}
